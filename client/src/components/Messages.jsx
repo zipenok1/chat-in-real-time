a8fd6e9e8bea0209b93a9f26caf03e00e5282fe8
zipenok1 <dmitrijkopejka5@gmail.com>
2025-01-07 01:49:50 +0300

    beta0.2

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -5,8 +5,6 @@ function Messages({messages, name}) {
     return (
         <div className="messages">
             {messages.map(({user, message}, i) =>{
-                // const itsMe = user.name.trim().toLowerCase() === name.trim().toLowerCase()   
-                // const className = itsMe ? document.body.style.me : document.body.style.user;
                 return(
                     <div key={i} className='user'>
                         <p>{user.name}</p>
